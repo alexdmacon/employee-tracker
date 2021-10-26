5d3608129a184305b9ca6a7204b246d00a9b3572
alexdmacon <alexdmacon@gmail.com>
2021-10-26 14:29:08 -0500

    working with inquirer prompts

--- server.js
+++ server.js
@@ -20,3 +20,28 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employees_db database.`)
 );
+
+const startTracker = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "employee_db",
+            message: "What do you want to do?",
+            choices: [
+                "View All Departments",
+                "View All Roles",
+                "View All Employees",
+                "Add a Department",
+                "Add a Role",
+                "Add an Employee",
+                "Update an Employee Role",
+                "Exit"
+            ]
+        }
+    ])
+    .then((answer) => {
+        if (answer === "View All Departments") {
+            showDepartments();
+        }
+    })
+}
