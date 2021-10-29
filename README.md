193da21baae5e66843ec0af5ea4f92bbad7da365
alexdmacon <alexdmacon@gmail.com>
2021-10-28 21:01:27 -0500

    trying to get inquirer answers to trigger database selection functions

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # employee-tracker
+
+
+
+
+https://www.npmjs.com/package/mysql2
+
+https://www.npmjs.com/package/console.table
+
+https://www.npmjs.com/package/inquirer
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mysql = require('mysql2');
-const inquirer = require('inquirer');
-const cTable = require('console.table');
+const express = require("express");
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -13,35 +13,73 @@ app.use(express.json());
 // Connect to database
 const db = mysql.createConnection(
   {
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'employees_db'
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "employees_db",
   },
   console.log(`Connected to the employees_db database.`)
 );
 
 const startTracker = () => {
-    inquirer.prompt([
-        {
-            type: "list",
-            name: "employee_db",
-            message: "What do you want to do?",
-            choices: [
-                "View All Departments",
-                "View All Roles",
-                "View All Employees",
-                "Add a Department",
-                "Add a Role",
-                "Add an Employee",
-                "Update an Employee Role",
-                "Exit"
-            ]
-        }
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "employee_db",
+        message: "What do you want to do?",
+        choices: [
+          "View All Departments",
+          "View All Roles",
+          "View All Employees",
+          "Add a Department",
+          "Add a Role",
+          "Add an Employee",
+          "Update an Employee Role",
+          "Exit",
+        ],
+      },
     ])
     .then((answer) => {
-        if (answer === "View All Departments") {
-            showDepartments();
-        }
-    })
-}
+        const { selection } = answer;
+
+      if (selection === "View All Departments") {
+          console.log("View All Departments")
+        viewDepartments();
+      }
+      if (answer === "View All Roles") {
+        viewRoles();
+      }
+      if (answer === "View All Employees") {
+        viewEmployees();
+      }
+      if (answer === "Add a Department") {
+        addDepartment();
+      }
+      if (answer === "Add an Employee") {
+        addEmployee();
+      }
+      if (answer === "Add a Role") {
+        addRole();
+      }
+      if (answer === "Update an Employee Role") {
+        updateRole();
+      }
+      if (answer === "Exit") {
+        db.end;
+      }
+    });
+};
+
+startTracker();
+
+const viewDepartments = () => {
+    console.log("Viewing all departments");
+
+    const sql = `SELECT department.id, department.name FROM department;`;
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+      } else console.table(rows);
+    });
+};
